15e263a642690959c30a5ac453d1dc8194ec3339
Anupriya53 <anupriyabhaskarkk08879@gmail.com>
2022-09-28 22:34:13 +0530

    fist commit

---
+++ string.py
@@ -0,0 +1,19 @@
+string="my first python program"
+print(string)
+print(len(string))
+print(string[3])
+print(string + ' is ')
+print(string.upper())
+print(string.startswith('hello'))
+print(string.endswith('program'))
+txt= " apple "
+x=txt.strip()
+print(x ,"is a fruit")
+a="python"
+print(a[1:4])
+print(a[1:])
+print(a[:])
+s='hello'
+print(s.replace('e','E'))
+
+
