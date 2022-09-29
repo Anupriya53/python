36434565ebdf62e493c3e403d9b641717170545a
Anupriya53 <anupriyabhaskarkk08879@gmail.com>
2022-09-29 21:46:45 +0530

    first commit

---
+++ list.py
@@ -0,0 +1,15 @@
+mylist=[1,4,6,7,8]
+print(mylist)
+print(mylist[2])
+s=mylist[2]+mylist[4]
+print(s)
+l1=["tomato","onion","carrot"]
+print(l1)
+l1[0]="cabbage"
+print(l1)
+print(l1[1])
+a= [9, 5, 21, 11]
+sum = 0
+for num in a:
+    sum += num
+print(sum)
