a6b406e1c7aef20e5760febd93cefbe7f66669b4
Anupriya53 <anupriyabhaskarkk08879@gmail.com>
2022-10-02 15:58:29 +0530

    commit

---
+++ stack1.py
@@ -0,0 +1,11 @@
+li=[]
+li.append('anu') #append() function to push elemnt in stack
+li.append('athu')
+li.append('akshi')
+print("initial stack:",li)
+print('\nelements popped from stack:')
+print(li.pop()) #pop 1
+print("peek:",li[-1]) #get top most element
+print(li.pop()) #pop 2
+print('\nstack after elements are popped:',li)
+               
